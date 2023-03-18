53ce2efc616614f89b1540859d95b5f1dcb7da41
User <User@DESKTOP-MANCTJR>
2023-03-18 21:46:59 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Operators
+{
+    class Program
+    {
+        static void Main()
+        {
+            RationalNumbers a = new RationalNumbers(2, 3);
+            RationalNumbers b = new RationalNumbers(3, 4);
+
+            RationalNumbers addition = a + b;
+            Console.WriteLine($"Результат сложения: {addition}\n");
+
+            RationalNumbers substraction = a - b;
+            Console.WriteLine($"Результат вычитания: {substraction}\n");
+
+            RationalNumbers multiplication = a * b;
+            Console.WriteLine($"Результат умножения: {multiplication}\n");
+
+            RationalNumbers division = a / b;
+            Console.WriteLine($"Результат деления: {division}\n");
+
+            bool isGreater = a > b;
+            Console.WriteLine($"Первая дробь больше второй? {isGreater}\n");
+
+            bool isLess = a < b;
+            Console.WriteLine($"Первая дробь меньше второй? {isLess}\n");
+
+            bool isEquals = a == b;
+            Console.WriteLine($"Обе дроби равны? {isEquals}\n");
+
+            bool isNotEquals = a != b;
+            Console.WriteLine($"Обе дроби не равны? {isNotEquals}\n");
+        }
+    }
+}
---
+++ RationalNumbers.cs
@@ -0,0 +1,81 @@
+﻿namespace Operators
+{
+    internal class RationalNumbers
+    {
+        public long numerator { get; }
+        public long denominator { get; }
+
+        public RationalNumbers(long numerator, long denominator)
+        {
+            if (denominator == 0)
+            {
+                throw new ArgumentException("Знаменатель не может быть равен нулю.");
+            }
+
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+
+        public static RationalNumbers operator +(RationalNumbers a, RationalNumbers b)
+        {
+            long numerator = a.numerator * b.denominator + b.numerator * a.denominator;
+            long commonDenominator = a.denominator * b.denominator;
+
+            return new RationalNumbers(numerator, commonDenominator);
+        }
+
+        public static RationalNumbers operator -(RationalNumbers a, RationalNumbers b)
+        {
+            long numerator = a.numerator * b.denominator - b.numerator * a.denominator;
+            long commonDenominator = a.denominator * b.denominator;
+
+            return new RationalNumbers(numerator, commonDenominator);
+        }
+
+        public static RationalNumbers operator *(RationalNumbers a, RationalNumbers b)
+        {
+            long numerator = a.numerator * b.numerator;
+            long denominator = a.denominator * b.denominator;
+
+            return new RationalNumbers(numerator, denominator);
+        }
+
+        public static RationalNumbers operator /(RationalNumbers a, RationalNumbers b)
+        {
+            if (b.numerator == 0)
+            {
+                throw new DivideByZeroException("Знаменатель не может быть равен нулю.");
+            }
+
+            long numerator = a.numerator * b.denominator;
+            long denominator = a.denominator * b.numerator;
+
+            return new RationalNumbers(numerator, denominator);
+        }
+
+        public static bool operator >(RationalNumbers a, RationalNumbers b)
+        {
+            return a.numerator * b.denominator > b.numerator * a.denominator;
+        }
+
+        public static bool operator <(RationalNumbers a, RationalNumbers b)
+        {
+            return a.numerator * b.denominator < b.numerator * a.denominator;
+        }
+
+        public static bool operator ==(RationalNumbers a, RationalNumbers b)
+        {
+            return a.numerator * b.denominator == b.numerator * a.denominator;
+        }
+
+        public static bool operator !=(RationalNumbers a, RationalNumbers b)
+        {
+            return a.numerator * b.denominator != b.numerator * a.denominator;
+        }
+
+        public override string ToString()
+        {
+            return $"{numerator}/{denominator}";
+        }
+    }
+}
